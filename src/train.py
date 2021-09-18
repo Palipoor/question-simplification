01c16d04513539b98ee78b87ae4b132b4a689877
Gabe <gabeorlanski@gmail.com>
2021-09-18 19:04:38 -0400

    Better args

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 import argparse
+
+import click
 import torch
 from tqdm import tqdm
 from transformers import (
@@ -11,32 +13,37 @@ from transformers import (
 from data.data_loaders import *
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--mode",
-        type=str,
-        help="train or evaluate mode",
-        required=True,
-        choices=["train", "evaluate"],
-    )
-    parser.add_argument("--dataset", type=str)
-    parser.add_argument("--model", type=str)
-    parser.add_argument("-load-ckpt", action="store_true")
-    parser.add_argument("--ckpt", type=str)
-    args = parser.parse_args()
-    MODEL_NAME = args.model
+@click.command()
+@click.argument("mode", metavar="<Mode>")
+@click.argument("dataset_name", metavar="<Dataset>")
+@click.argument("model_name", metavar="<model_name>")
+@click.option("--batch-size", "-bs", type=int, default=16)
+def main(mode, dataset_name, model_name, batch_size):
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     "--mode",
+    #     type=str,
+    #     help="train or evaluate mode",
+    #     required=True,
+    #     choices=["train", "evaluate"],
+    # )
+    # parser.add_argument("--dataset", type=str)
+    # parser.add_argument("--model", type=str)
+    # parser.add_argument("-load-ckpt", action="store_true")
+    # parser.add_argument("--ckpt", type=str)
+    # args = parser.parse_args()
 
-    model = AutoModel.from_pretrained(MODEL_NAME)
-    if args.load_ckpt:
-        pass  # TODO load from checkpoint
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-    training_arguments = Seq2SeqTrainingArguments(  # TODO update these. Just copied from another code I had lol
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    # if args.load_ckpt:
+    #     pass  # TODO load from checkpoint
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    training_arguments = Seq2SeqTrainingArguments(
+        # TODO update these. Just copied from another code I had lol
         output_dir="./output/",
         do_train=True,
         do_eval=True,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
         num_train_epochs=50,
         logging_steps=500,
         logging_first_step=True,
@@ -46,8 +53,8 @@ def main():
         save_total_limit=2,
         learning_rate=1e-5,
     )
-    train_dataset = get_dataset(args.dataset, tokenizer, "train")
-    eval_dataset = get_dataset(args.dataset, tokenizer, "eval")
+    train_dataset = get_dataset(dataset_name, tokenizer, "train")
+    eval_dataset = get_dataset(dataset_name, tokenizer, "eval")
     print(eval_dataset)
     trainer = Seq2SeqTrainer(
         model,
