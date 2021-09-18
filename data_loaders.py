db32ba784a24151b4f183dc249f990ecd42a688e
Pegah Alipoormolabashi <palipoor976@gmail.com>
2021-09-18 14:09:15 -0700

    Add dataloader for turk

--- data_loaders.py
+++ data_loaders.py
@@ -1,4 +1,5 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
+from numpy import load
 
 def get_asset_data():
     pass
@@ -10,5 +11,30 @@ def get_zest_data():
     pass
 
 
-def get_dataset(dataset_name, split):
+def get_turk_data(split):   
+    """
+    returns a dataset like:
+    Dataset({
+    features: ['original', 'simplification'],
+    num_rows: 2872
+    })
+    
+    """
+    if split == 'train':
+        turk_split = 'evaluation'
+    else:
+        turk_split = 'test'
+    turk_data = load_dataset('turk')[turk_split]
+    instances = {
+        'original': [],
+        'simplification': []
+    }
+    for instance in turk_data:
+        for simplification in instance['simplifications']:
+            instances['original'].append(instance['original'])
+            instances['simplification'].append(simplification)
+    dataset = Dataset.from_dict(instances)
+    return dataset
+    
+def get_dataset(dataset_name,tokenizer, split):
     pass
