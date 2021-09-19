cb1c239e4b63fd92503961c27ef140729a1a69c1
Pegah Alipoormolabashi <palipoor976@gmail.com>
2021-09-18 17:43:26 -0700

    Add bleu

--- src/train.py
+++ src/train.py
@@ -2,15 +2,17 @@ import argparse
 
 import click
 import torch
+import functools
 from tqdm import tqdm
 from transformers import (
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
     Seq2SeqTrainer,
     Seq2SeqTrainingArguments,
-    training_args,
+    DataCollatorForSeq2Seq
 )
 from data.data_loaders import *
+from utils import *
 
 
 @click.command()
@@ -32,7 +34,6 @@ def main(mode, dataset_name, model_name, batch_size):
     # parser.add_argument("-load-ckpt", action="store_true")
     # parser.add_argument("--ckpt", type=str)
     # args = parser.parse_args()
-
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     # if args.load_ckpt:
     #     pass  # TODO load from checkpoint
@@ -46,21 +47,27 @@ def main(mode, dataset_name, model_name, batch_size):
         per_device_eval_batch_size=batch_size,
         num_train_epochs=50,
         logging_steps=500,
+        eval_steps = 100,
         logging_first_step=True,
         save_steps=500,
         evaluation_strategy="steps",
         run_name="fooo",
         save_total_limit=2,
-        learning_rate=1e-5,
+        learning_rate=2e-5,
+        predict_with_generate=True,
+        do_predict= True
     )
     train_dataset = get_dataset(dataset_name, tokenizer, "train")
     eval_dataset = get_dataset(dataset_name, tokenizer, "eval")
-    print(eval_dataset)
+    data_collator = DataCollatorForSeq2Seq(tokenizer =tokenizer)
     trainer = Seq2SeqTrainer(
         model,
         args=training_arguments,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
+        compute_metrics=functools.partial(compute_metrics_bleu,tokenizer=tokenizer),
+        tokenizer = tokenizer,
+        data_collator=data_collator
     )
     trainer.train()
 
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,35 @@
 import numpy as np
 from transformers import EvalPrediction
+from datasets import load_metric
 from typing import Dict
 
 
-def compute_metrics_bleu(p: EvalPrediction) -> Dict:
-    pass
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [[label.strip()] for label in labels]
 
+    return preds, labels
+
+def compute_metrics_bleu(prediction, tokenizer):
+    metric = load_metric("sacrebleu")
+    preds, labels = prediction
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # Some simple post-processing
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    result = {"bleu": result["score"]}
+
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+    result["gen_len"] = np.mean(prediction_lens)
+    result = {k: round(v, 4) for k, v in result.items()}
+    return result
 
 def compute_metrics_sari(p: EvalPrediction) -> Dict:
     pass
