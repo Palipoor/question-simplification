16832a2f71a696870208df2eaa7cd62b1ba5675f
Pegah Alipoormolabashi <palipoor976@gmail.com>
2021-09-18 18:38:37 -0700

    add tensorboard reports

--- train.py
+++ train.py
@@ -20,26 +20,11 @@ from src.utils import *
 @click.argument("dataset_name", metavar="<Dataset>")
 @click.argument("model_name", metavar="<model_name>")
 @click.option("--batch-size", "-bs", type=int, default=16)
-def main(mode, dataset_name, model_name, batch_size):
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "--mode",
-    #     type=str,
-    #     help="train or evaluate mode",
-    #     required=True,
-    #     choices=["train", "evaluate"],
-    # )
-    # parser.add_argument("--dataset", type=str)
-    # parser.add_argument("--model", type=str)
-    # parser.add_argument("-load-ckpt", action="store_true")
-    # parser.add_argument("--ckpt", type=str)
-    # args = parser.parse_args()
+@click.option("--checkpoint", "-chkpt", type = str, default = None)
+def main(mode, dataset_name, model_name, batch_size, checkpoint):
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    # if args.load_ckpt:
-    #     pass  # TODO load from checkpoint
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     training_arguments = Seq2SeqTrainingArguments(
-        # TODO update these. Just copied from another code I had lol
         output_dir="./output/",
         do_train=True,
         do_eval=True,
@@ -47,16 +32,16 @@ def main(mode, dataset_name, model_name, batch_size):
         per_device_eval_batch_size=batch_size,
         num_train_epochs=50,
         logging_steps=500,
-        eval_steps = 100,
+        evaluation_strategy = "epoch",
         logging_first_step=True,
-        save_steps=500,
-        evaluation_strategy="steps",
-        run_name="fooo",
         save_total_limit=2,
         learning_rate=2e-5,
         predict_with_generate=True,
-        do_predict= True
+        do_predict= True,
+        report_to= ["tensorboard"]
     )
+    if checkpoint:
+        training_arguments.resume_from_checkpoint = checkpoint
     train_dataset = get_dataset(dataset_name, tokenizer, "train")
     eval_dataset = get_dataset(dataset_name, tokenizer, "eval")
     data_collator = DataCollatorForSeq2Seq(tokenizer =tokenizer)
